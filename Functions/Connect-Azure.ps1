3b182f6bc82f912caa7580848c3d8466730ea195
hhazeley <hhazeley@hazelnest.com>
2018-01-20 17:56:33 -0800

    Update to 1.0.4
    --Added Help to Function
    --Remove the option to check for a deafult 'azurecred.xml' file

--- Functions/Connect-Azure.ps1
+++ Functions/Connect-Azure.ps1
@@ -1,5 +1,28 @@
 Function Connect-Azure
 {
+
+<#
+  .SYNOPSIS
+  Connect to Azure 
+  
+  .DESCRIPTION
+  This cmdlet allows you to connect to Azure using stored credentials
+    
+  .PARAMETER Path
+  Path to the xml stored credential
+
+  .NOTES
+  Author     : Hannel Hazeley - hhazeley@outlook.com
+
+  .LINK
+  https://github.com/hhazeley/HannelsToolBox/blob/master/Functions/Connect-Azure.ps1
+
+  .EXAMPLE
+  Connect-Azure -Path C:\Secure\AzureCred.xml
+  
+  Connects to Azure using credential stored in xml file. 
+  #>
+
  Param(
     $Path
    )
@@ -9,24 +32,15 @@ Function Connect-Azure
 
    if ($Path -eq $null)
    {
-       $cred = Import-Clixml C:\Scripts\azurecred.xml
-       if ($cred -eq $null -or $cred.UserName -eq $null)
-       {
        Login-AzureRmAccount -ErrorVariable LoginError
        Write-Host -ForegroundColor Red $LoginError
-       }
-       else 
-       {
-       Login-AzureRmAccount -Credential $cred -ErrorVariable LoginError
-       Write-Host -ForegroundColor Red $LoginError
-       }
    }
    else 
    {
        $cred = Import-Clixml $Path
        if ($cred -eq $null -or $cred.UserName -eq $null)
        {
-       Write-Host -ForegroundColor Cyan "No credential in file, please provide credential"
+       Write-Host -ForegroundColor Red "No credential in file, please provide credential"
        Login-AzureRmAccount -ErrorVariable LoginError
        Write-Host -ForegroundColor Red $LoginError
        }
@@ -36,7 +50,6 @@ Function Connect-Azure
        Write-Host -ForegroundColor Red $LoginError
        }
    }
-
 
 }
 
