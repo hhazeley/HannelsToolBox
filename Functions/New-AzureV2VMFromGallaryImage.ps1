91744b3f127888a47484b9b32a8d654cc7be0ace
hhazeley <hhazeley@hazelnest.com>
2018-01-20 17:56:52 -0800

    Update to 1.0.4
    --Added Help to Function
    --Added more to the validation set for location, vmSize and windowsSku
    --Change the backup disk naming format to prevent very log names
    --Change the VMUsername and VMPassword parameters to aling with
    ScriptAnalyzer rules
    --Change the Function's defaults
    --Change Storage account nameing for a more unique name creation
    --Remove unused lines from function

--- Functions/New-AzureV2VMFromGallaryImage.ps1
+++ Functions/New-AzureV2VMFromGallaryImage.ps1
@@ -1,5 +1,58 @@
 Function New-AzureV2VMFromGallaryImage
 {
+
+<#
+  .SYNOPSIS
+  Creating virtual machine(s) from gallery image with one line.
+  
+  .DESCRIPTION
+  This cmdlet will create a single or numerous virtual machines from gallery image with just one line. 
+  
+  .PARAMETER SubscriptionId
+  Subscription ID for the subscription that virtual machine(s) is on. Required
+    
+  .PARAMETER rgName
+  Resource group where the resources will be created. Required
+
+  .PARAMETER vmNames
+  Name or names of the virtual machine(s) to be created. Required
+
+  .PARAMETER location
+  Location where the Azure resources will be created.
+
+  .PARAMETER saName
+  Name of the storage account name to use if you want to use un-managed disk. Note: If storage account doesn't exist, cmdlet will create a unique storage account.
+
+  .PARAMETER vnetName
+  Name of the virtual network that virtual machine will be on.
+
+  .PARAMETER vmSize
+  Size of the new virtual machine that is been created Example: Standard_A0 See information in link https://azure.microsoft.com/en-us/documentation/articles/virtual-machines-windows-sizes/.
+
+  .PARAMETER WindowsSku
+  Windows SKU to use to create the virtual machine(s).
+
+  .PARAMETER nsgName
+  Specify a NIC NSG that you will like machine to be on. If not specified a Windows default NSG will be used/created automatically.
+
+  .PARAMETER VMUser
+  Username that will be used for the Windows configuration.
+
+  .PARAMETER VMPass
+  Password that will be used for the Windows configuration. If not specified a strong random password will be generated and used.
+
+  .NOTES
+  Author     : Hannel Hazeley - hhazeley@outlook.com
+
+  .LINK
+  https://github.com/hhazeley/HannelsToolBox/blob/master/Functions/New-AzureV2VMFromGallaryImage.ps1
+
+  .EXAMPLE
+  New-AzureV2VMFromGallaryImage -SubscriptionId 1d6737e7-4f6c-4e3c-8cd4-996b6f003d0e -rgName DVideoRG1 -vmNames DV1-DPBSV1-001
+
+  This will create virtual machine using all cmdlet default configuration.
+  #>
+
 [cmdletbinding()]
 Param (
     [Parameter(Mandatory=$true)]
@@ -8,17 +61,17 @@ Param (
     $rgName,
     [Parameter(Mandatory=$true)]
     $vmNames,
-    [ValidateSet('eastasia','southeastasia','centralus','eastus','eastus2','westus','northcentralus','southcentralus','northeurope','westeurope','japanwest','japaneast','brazilsouth','australiaeast','australiasoutheast','southindia','centralindia','westindia','canadacentral','canadaeast','uksouth','ukwest','westcentralus','westus2')]
+    [ValidateSet('eastasia','southeastasia','centralus','eastus','eastus2','westus','northcentralus','southcentralus','northeurope','westeurope','japanwest','japaneast','brazilsouth','australiaeast','australiasoutheast','southindia','centralindia','westindia','canadacentral','canadaeast','uksouth','ukwest','westcentralus','westus2','koreacentral','koreasouth')]
     $location,
     $saName,
     $vnetName,
-    [ValidateSet('Standard_A0','Standard_A1','Standard_A2','Standard_A3','Standard_A5','Standard_A4','Standard_A6','Standard_A7','Basic_A0','Basic_A1','Basic_A2','Basic_A3','Basic_A4','Standard_D1_v2','Standard_D2_v2','Standard_D3_v2','Standard_D4_v2','Standard_D5_v2','Standard_D11_v2','Standard_D12_v2','Standard_D13_v2','Standard_D14_v2','Standard_D15_v2','Standard_F1','Standard_F2','Standard_F4','Standard_F8','Standard_F16','Standard_A1_v2','Standard_A2m_v2','Standard_A2_v2','Standard_A4m_v2','Standard_A4_v2','Standard_A8m_v2','Standard_A8_v2','Standard_DS1_v2','Standard_DS2_v2','Standard_DS3_v2','Standard_DS4_v2','Standard_DS5_v2','Standard_DS11_v2','Standard_DS12_v2','Standard_DS13_v2','Standard_DS14_v2','Standard_DS15_v2','Standard_F1s','Standard_F2s','Standard_F4s','Standard_F8s','Standard_F16s','Standard_D1','Standard_D2','Standard_D3','Standard_D4','Standard_D11','Standard_D12','Standard_D13','Standard_D14','Standard_DS1','Standard_DS2','Standard_DS3','Standard_DS4','Standard_DS11','Standard_DS12','Standard_DS13','Standard_DS14','Standard_G1','Standard_G2','Standard_G3','Standard_G4','Standard_G5','Standard_GS1','Standard_GS2','Standard_GS3','Standard_GS4','Standard_GS5','Standard_A8','Standard_A9','Standard_A10','Standard_A11','Standard_H8','Standard_H16','Standard_H8m','Standard_H16m','Standard_H16r','Standard_H16mr')]
+    [ValidateSet('Standard_DS1_v2','Standard_DS2_v2','Standard_DS3_v2','Standard_DS4_v2','Standard_DS5_v2','Standard_DS11_v2','Standard_DS12_v2','Standard_DS13-2_v2','Standard_DS13-4_v2','Standard_DS13_v2','Standard_DS14-4_v2','Standard_DS14-8_v2','Standard_DS14_v2','Standard_DS15_v2','Standard_F1s','Standard_F2s','Standard_F4s','Standard_F8s','Standard_F16s','Standard_A0','Standard_A1','Standard_A2','Standard_A3','Standard_A5','Standard_A4','Standard_A6','Standard_A7','Basic_A0','Basic_A1','Basic_A2','Basic_A3','Basic_A4','Standard_D1_v2','Standard_D2_v2','Standard_D3_v2','Standard_D4_v2','Standard_D5_v2','Standard_D11_v2','Standard_D12_v2','Standard_D13_v2','Standard_D14_v2','Standard_D15_v2','Standard_DS1','Standard_DS2','Standard_DS3','Standard_DS4','Standard_DS11','Standard_DS12','Standard_DS13','Standard_DS14','Standard_B1ms','Standard_B1s','Standard_B2ms','Standard_B2s','Standard_B4ms','Standard_B8ms','Standard_D2_v3','Standard_D4_v3','Standard_D8_v3','Standard_D16_v3','Standard_D32_v3','Standard_D64_v3','Standard_D2s_v3','Standard_D4s_v3','Standard_D8s_v3','Standard_D16s_v3','Standard_D32s_v3','Standard_D64s_v3','Standard_E2_v3','Standard_E4_v3','Standard_E8_v3','Standard_E16_v3','Standard_E32_v3','Standard_E64_v3','Standard_E2s_v3','Standard_E4s_v3','Standard_E8s_v3','Standard_E16s_v3','Standard_E32-8s_v3','Standard_E32-16s_v3','Standard_E32s_v3','Standard_E64-16s_v3','Standard_E64-32s_v3','Standard_E64s_v3','Standard_G1','Standard_G2','Standard_G3','Standard_G4','Standard_G5','Standard_GS1','Standard_GS2','Standard_GS3','Standard_GS4','Standard_GS4-4','Standard_GS4-8','Standard_GS5','Standard_GS5-8','Standard_GS5-16','Standard_L4s','Standard_L8s','Standard_L16s','Standard_L32s','Standard_A8','Standard_A9','Standard_A10','Standard_A11','Standard_H8','Standard_H16','Standard_H8m','Standard_H16m','Standard_H16r','Standard_H16mr')]
     $vmSize,
-    [ValidateSet('2008-R2-SP1','2008-R2-SP1-BYOL','2012-Datacenter','2012-Datacenter-BYOL','2012-R2-Datacenter','2012-R2-Datacenter-BYOL','2016-Datacenter','2016-Datacenter-Server-Core','2016-Datacenter-with-Containers','2016-Nano-Server')]
+    [ValidateSet('2008-R2-SP1','2008-R2-SP1-smalldisk','2012-Datacenter','2012-Datacenter-smalldisk','2012-R2-Datacenter','2012-R2-Datacenter-smalldisk','2016-Datacenter','2016-Datacenter-smalldisk')]
     $WindowsSku,
     $nsgName,
-    $VMUsername,
-    $VMPassword
+    $VMUser,
+    $VMPass
 )
 
 $ErrorActionPreference = "SilentlyContinue"
@@ -26,7 +79,7 @@ $WarningPreference = "SilentlyContinue"
 
 If ($location -eq $null)
 {
-$location = "westus"
+$location = "westus2"
 }
 If ($vnetName -eq $null)
 {
@@ -34,11 +87,11 @@ $vnetName = $rgName+"-VNet1"
 }
 If ($vmSize -eq $null)
 {
-$vmSize = "Standard_A2_v2"
+$vmSize = "Standard_B2ms"
 }
 If ($WindowsSku -eq $null)
 {
-$WindowsSku = "2012-R2-Datacenter"
+$WindowsSku = "2012-R2-Datacenter-smalldisk"
 }
 If ($StorageSku -eq $null)
 {
@@ -80,6 +133,16 @@ if ($VNetcreated.IsPresent)
 Write-Host -ForegroundColor Yellow -BackgroundColor Black "Rolling back..... Removing Virtual Network $vnetName"
 Remove-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName -Force | Out-Null
 }
+if ($sacreated.IsPresent)
+{
+Write-Host -ForegroundColor Yellow -BackgroundColor Black "Rolling back..... Removing Storage Account $saName"
+Remove-AzureRmStorageAccount -Name $saName -ResourceGroupName $rgName -Force | Out-Null
+}
+if ($rgcreated.IsPresent)
+{
+Write-Host -ForegroundColor Yellow -BackgroundColor Black "Rolling back..... Removing Resource Group $rgName"
+Remove-AzureRmResourceGroup -Name $rgName -Force | Out-Null
+}
 Write-Host "______________________________________________________________________"
 Write-Host -ForegroundColor Red "Script aborted and actions rolled back, see above error."
 Write-Host "______________________________________________________________________"
@@ -117,14 +180,28 @@ $location = $rgLocation
 
 If ($saName -ne $null)
 {
-$saName = $saName -replace '[^a-zA-Z0-9]', ''
-$saName = $saName.ToLower() +"str1"
 
 #Prepare storage account for virtual machine
 $storageAccValidation = Get-AzureRmStorageAccount -ResourceGroupName $rgName -AccountName $saName
 if ($storageAccValidation -eq $null)
 {
 # Create a new storage account for the VM
+
+$rnum = Get-Random -Minimum 1000 -Maximum 9999
+$userID = (Get-AzureRmContext).Account.Id
+$userID = $userID.Substring(0,3)
+if ($rgName.Length -gt 14)
+{
+$rgNamestr = $rgName.Substring(0,14)
+}
+else
+{
+$rgNamestr = $rgName
+}
+$saName = $rgNamestr+"str"+$userID+$rnum 
+$saName = $saName -replace '[^a-zA-Z0-9]', ''
+$saName = $saName.ToLower()
+
 Write-Host -ForegroundColor Green "Creating Storage Account $saName"
 $storageAcc = New-AzureRmStorageAccount -Location $location -Name $saName -ResourceGroupName $rgName -SkuName $StorageSku -Kind Storage -ErrorVariable errorck
 ErrorCheck
@@ -163,25 +240,18 @@ Break
 }
 Write-Host -ForegroundColor Green "Starting deployment for Virtual Machine $vmName."
 
-If ($VMUsername -eq $null)
+If ($VMUser -eq $null)
 {
 $VMUser = "VMAdmin"
 }
-Else
-{
-$VMUser = $VMUsername
-}
-If ($VMPassword -eq $null)
+
+If ($VMPass -eq $null)
 {
 [Reflection.Assembly]::LoadWithPartialName("System.Web") | Out-Null
-$Pword = [System.Web.Security.Membership]::GeneratePassword(15,2)
-}
-Else
-{
-$Pword = $VMPassword
+$VMPass = [System.Web.Security.Membership]::GeneratePassword(15,2)
 }
 
-$VMPWord = ConvertTo-SecureString -String "$Pword" -AsPlainText -Force
+$VMPWord = ConvertTo-SecureString -String "$VMPass" -AsPlainText -Force
 $cred = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $VMUser, $VMPWord
 
 #Getting existing NSG in resource group
@@ -199,20 +269,10 @@ If ($VNetworkSecurityGroup -notcontains "$vmnsgName")
 {
 #Creating Default Windows NSG
 Write-Host -ForegroundColor Green "Creating Network Security Groups $vmnsgName"
-If ($WindowsSku -ne "2016-Nano-Server")
-{
 $NSGRule = New-AzureRmNetworkSecurityRuleConfig -Name default-allow-rdp -Access Allow -Description "Allowing RDP connection" -DestinationAddressPrefix * -DestinationPortRange 3389 -Direction Inbound -Priority 1000 -Protocol Tcp -SourceAddressPrefix * -SourcePortRange *
 New-AzureRmNetworkSecurityGroup -Location $location -Name $vmnsgName -ResourceGroupName $rgName -SecurityRules $NSGRule | Out-Null
 [Switch]$nsgcreated = $true
 ErrorCheck
-}
-else
-{
-$NSGRule = New-AzureRmNetworkSecurityRuleConfig -Name WinRM -Access Allow -Description "Allowing WinRM connection" -DestinationAddressPrefix * -DestinationPortRange 5985-5986 -Direction Inbound -Priority 1000 -Protocol Tcp -SourceAddressPrefix * -SourcePortRange *
-New-AzureRmNetworkSecurityGroup -Location $location -Name $vmnsgName -ResourceGroupName $rgName -SecurityRules $NSGRule | Out-Null
-[Switch]$nsgcreated = $true
-ErrorCheck
-}
 }
 
 #Setting NSG to windows default NSG, since its not provided
@@ -259,23 +319,16 @@ $vm = Set-AzureRmVMOSDisk -VM $vm -Name $osDiskName -VhdUri $osDiskUri -CreateOp
 }
 #Create the new VM
 Write-Host -ForegroundColor Green "Creating Virtual Machine $vmName"
-If ($WindowsSku -ne "2016-Nano-Server")
-{
 $newVM = New-AzureRmVM -ResourceGroupName $rgName -Location $location -VM $vm -ErrorVariable errorck
 ErrorCheck
-}
-else
-{
-$newVM = New-AzureRmVM -ResourceGroupName $rgName -Location $location -VM $vm -DisableBginfoExtension -ErrorVariable errorck
-ErrorCheck
-}
+
 $vmIP = (Get-AzureRmPublicIpAddress -Name $ipName1 -ResourceGroupName $rgName).IpAddress
 Write-Host "______________________________________________________________________"
 Write-Host -ForegroundColor Green "Virtual Machine $vmName deployment completed."
 Write-Host -ForegroundColor Green "Username: " -NoNewline
 Write-Host -ForegroundColor White $VMUser -NoNewline
 Write-Host -ForegroundColor Green " Password: " -NoNewline
-Write-Host -ForegroundColor White $Pword -NoNewline
+Write-Host -ForegroundColor White $VMPass -NoNewline
 Write-Host -ForegroundColor Green " PublicIP: " -NoNewline
 Write-Host -ForegroundColor White $vmIP
 Write-Host "______________________________________________________________________"
